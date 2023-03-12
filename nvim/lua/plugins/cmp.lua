10367eb43968ad7760417b62965b2313a1c27777
temso <temsolv@gmail.com>
2023-03-13 03:41:49 +0900

    new keybinds

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -2,6 +2,8 @@ local cmp = require('cmp')
 
 cmp.setup({
 	mapping = cmp.mapping.preset.insert({
+		['<C-k>'] = cmp.mapping.select_prev_item(),
+		['<C-j>'] = cmp.mapping.select_next_item(),
 		['<C-b>'] = cmp.mapping.scroll_docs(-4),
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
 		['<C-o>'] = cmp.mapping.complete(),
