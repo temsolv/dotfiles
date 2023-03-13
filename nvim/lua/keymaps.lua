4fc99d7163bd9fc456aafe39b41fb2199767a3f5
temso <temsolv@gmail.com>
2023-03-14 00:53:28 +0900

    minor changes

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,8 +1,6 @@
--- alias
 local map = vim.api.nvim_set_keymap
 local default_opts = {noremap = true, silent = true}
 
--- maps
 map('i', 'jk', '<esc>', {noremap = true})
 map('n', '<C-f>', '<cmd>NvimTreeToggle<cr>', default_opts)
 map('n', ',<space>', ':nohlsearch<CR>', default_opts)
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,16 +1,12 @@
--- alias
 local opt = vim.opt
 local cmd = vim.cmd
 
--- tabulation
 opt.shiftwidth = 4
 opt.tabstop = 4
 opt.smartindent = true
 
--- view 
 opt.number = true
 opt.so = 5 
-cmd ('set noshowmode')
+cmd ('set noshowmode') 
 
--- edit
-cmd ('autocmd BufEnter * set fo-=c fo-=r fo-=o')
+cmd ('autocmd BufEnter * set fo-=c fo-=r fo-=o') 
