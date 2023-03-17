071bee4d231db7d2f4cd1e88efe9cb2f978a1c45
temso <temsolv@gmail.com>
2023-03-17 17:15:23 +0900

    file reorganisation

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,6 @@
+-- init packer
+require('packer_init')
+
 -- options
 require('options/init')
 
@@ -5,10 +8,4 @@ require('options/init')
 require('lsp/init')
 
 -- plugins
-require('plugins/lualine')
-require('plugins/nvim-tree')
-require('plugins/autopairs')
-require('plugins/luasnip')
-require('plugins/devicons')
-require('plugins/bufferline')
-require('plugins/comment')
+require('plugins/init')
